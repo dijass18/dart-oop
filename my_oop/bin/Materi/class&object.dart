b175451008dc2c88ea7145e3418f02d9494e0b1d
dijass18 <muhdijas18@gmail.com>
2024-03-24 13:43:46 +0700

    Constructor

--- my_oop/bin/Materi/class&object.dart
+++ my_oop/bin/Materi/class&object.dart
@@ -20,5 +20,20 @@ void main(){
     print(mobil2.merk); 
     print(mobil2.model); 
     print(mobil2.tahun); 
+  print("");
+
+    PersegiPanjang persegiPanjang = PersegiPanjang();
+    persegiPanjang.panjang = 10;
+    persegiPanjang.lebar = 5;
+    print("Panjang Persegi Adalah 10");
+    print("Panjang Lebar Adalah 5");
+    print("Luas Persegi Panjang adalah ${persegiPanjang.area()}.");
 }
 
+class PersegiPanjang {
+    double? panjang;
+    double? lebar;
+    double area(){
+        return panjang! * lebar!;
+    }
+}
