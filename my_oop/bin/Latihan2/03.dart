21708d2706a94bf00d0921a9029b7df6ea774a5a
dijass18 <muhdijas18@gmail.com>
2024-03-24 15:03:13 +0700

    Latihan 2 Soal 3

--- my_oop/bin/Latihan2/03.dart
+++ my_oop/bin/Latihan2/03.dart
@@ -1,41 +1,27 @@
-import 'dart:math';
-
 class BangunDatar {
-  double hitungLuas() {
-    return 0;
+  void hitungLuas() {
+    print('bangun datar');
   }
 }
 
 class Persegi extends BangunDatar {
-  double sisi;
-
-  Persegi(this.sisi);
-
   @override
-  double hitungLuas() {
-    return sisi * sisi;
+  void hitungLuas() {
+    print('Menghitung Luas Persegi = Sisi x Sisi');
   }
 }
 
 class Segitiga extends BangunDatar {
-  double alas, tinggi;
-
-  Segitiga(this.alas, this.tinggi);
-
   @override
-  double hitungLuas() {
-    return 0.5 * alas * tinggi;
+  void hitungLuas() {
+    print('Menghitung Luas Segitiga = 1/2 x alas x tinggi');
   }
 }
 
 void main() {
-  Persegi persegi = Persegi(5);
-  double luasPersegi = persegi.hitungLuas();
-  print("Panjang sisi : ${persegi.sisi}");
-  print("Luas Persegi (Sisi x Sisi ) : $luasPersegi");
+  Persegi persegi = Persegi();
+  Segitiga segitiga = Segitiga();
 
-  Segitiga segitiga = Segitiga(3, 4);
-  double luasSegitiga = segitiga.hitungLuas();
-  print("Alas : ${segitiga.alas}, Tinggi : ${segitiga.tinggi}");
-  print("Luas Segitiga (1/2 Alas x Tinggi): $luasSegitiga");
+  persegi.hitungLuas();
+  segitiga.hitungLuas();
 }
